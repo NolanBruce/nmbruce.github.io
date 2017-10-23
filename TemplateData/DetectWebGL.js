eb763a4a6266bae614f1176ec07c6b51ed1f0a1c
nmbruce <nmbruce@asu.edu>
2017-10-23 13:11:45 -0400

    Testing webgl detection

---
+++ TemplateData/DetectWebGL.js
@@ -0,0 +1,19 @@
+﻿function detectWebGLSupport() {
+    //Create canvas for getting context
+    var canvas = document.createElement("canvas");
+    //Get webgl Context from canvas
+    /* NOTE: removed experimental-webgl context to avoid
+    accidental approval of older mobile devices. Remove
+    commented out code to reactivate.*/
+    var gl = !! canvas.getContext("webgl");// || !! canvas.getContext("experimental-webgl");
+    //Get WebGLRenderingContext from window
+    var rendcon = !! window.WebGLRenderingContext;
+    //
+    if (rendcon && gl) {
+    	window.alert("Good to go.");
+    } else {
+    	window.alert("No good, man.");
+    }
+  }
+
+  detectWebGLSupport();
