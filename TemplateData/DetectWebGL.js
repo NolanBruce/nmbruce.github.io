aaac9118d9a9ab9e2c578211510df83660b5eb3f
nmbruce <nmbruce@asu.edu>
2017-10-26 12:28:34 -0400

    Just a 5 minute rebuild because of a typo...

--- TemplateData/DetectWebGL.js
+++ TemplateData/DetectWebGL.js
@@ -10,7 +10,7 @@
     var rendcon = !! window.WebGLRenderingContext;
     //if webgl context or rendering context
     if (rendcon || gl) {
-    	if (onfirm("Small Worlds cannot run because" + 
+    	if (confirm("Small Worlds cannot run because" + 
             "WebGL is not supported on your device.") {
     		window.location = "https://etx.asu.edu/";
     	} else {
