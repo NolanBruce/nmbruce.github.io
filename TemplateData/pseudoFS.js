56d5395f251c3ef6a068d83b8b6870ca326471ef
nmbruce <nmbruce@asu.edu>
2018-02-15 14:58:49 -0500

    Testing new scrollTo() methodology

--- TemplateData/pseudoFS.js
+++ TemplateData/pseudoFS.js
@@ -1,5 +1,10 @@
 function pseudoFS() {
-	setTimeout(function () {
-  		window.scrollTo(0, 1);
-	}, 20000);
+	window.addEventListener("load", function() {
+	  setTimeout(function() {
+	    var scrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+	    if (scrollPos < 1) {
+	      window.scrollTo(0,1);
+	    }
+	  }, 0);
+	});
 }
