9bcad92d90ed60e8ef5aa9f12cc2793d715eccdf
nmbruce <nmbruce@asu.edu>
2018-03-02 16:21:42 -0500

    Fixing error

--- TemplateData/fullscreen.js
+++ TemplateData/fullscreen.js
@@ -1,7 +1,7 @@
 function fullscreen() {
 	var iframes = document.getElementsByTagName('iframe');
 	console.log(iframes);
-	document.body.addEventListener('DOMNodeInserted', function( event ) {
+	window.onload = document.body.addEventListener('DOMNodeInserted', function( event ) {
     console.log('whoot! a new Element was inserted, see my event object for details!');
 	}, false);
 }
