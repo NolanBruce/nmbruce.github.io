da412e4bb5b471e08f6422dd950b8152c374d4a7
nmbruce <nmbruce@asu.edu>
2018-03-02 16:06:40 -0500

    Trying new timeout method...

--- TemplateData/fullscreen.js
+++ TemplateData/fullscreen.js
@@ -1,4 +1,9 @@
 function fullscreen() {
+	var timeout;
+	timeout = window.onload(goFull(), 300000);
+}
+
+function goFull() {
 	var frame = document.getElementById("aelp");
 	frame.requestFullscreen();
 }
