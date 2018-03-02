ae2d6c68699be672587d02fa7716e054f4adbb25
nmbruce <nmbruce@asu.edu>
2018-03-02 16:20:07 -0500

    Testing event handling with element additions

--- TemplateData/fullscreen.js
+++ TemplateData/fullscreen.js
@@ -1,6 +1,9 @@
 function fullscreen() {
 	var iframes = document.getElementsByTagName('iframe');
 	console.log(iframes);
+	document.body.addEventListener('DOMNodeInserted', function( event ) {
+    console.log('whoot! a new Element was inserted, see my event object for details!');
+	}, false);
 }
 
 function goFull() {
