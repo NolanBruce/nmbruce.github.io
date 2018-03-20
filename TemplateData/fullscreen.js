36979f4153bbad07c58130d86b84a8d9c8982e65
nmbruce <nmbruce@asu.edu>
2018-03-20 10:47:01 -0400

    Adding fullscreen back to index to experiment with maniputating div.

---
+++ TemplateData/fullscreen.js
@@ -0,0 +1,23 @@
+function fullscreen() {
+		goFull();
+}
+
+function goFull() {
+	var frame = document.getElementById("gameContainer");
+	try {
+		frame.requestFullscreen();
+	} catch(err) {
+		console.log("requestFullScreen failed");
+	}
+	try {
+		frame.mozRequestFullscreen();
+	} catch(err) {
+		console.log("mozRequestFullScreen failed");
+	}
+	try {
+		frame.webkitRequestFullscreen();
+	} catch(err) {
+		console.log("webkitRequestFullScreen failed");
+	}
+	
+}
