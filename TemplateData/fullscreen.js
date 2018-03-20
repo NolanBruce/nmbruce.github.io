f725afd35b1854a994eaa466a86e21ebceac4028
nmbruce <nmbruce@asu.edu>
2018-03-20 12:48:15 -0400

    Syntax fix2

--- TemplateData/fullscreen.js
+++ TemplateData/fullscreen.js
@@ -40,7 +40,7 @@ function addFullListener() {
 function addButton(e) {
 	console.log("x= " + e.clientX);
 	console.log("y= " + e.clientY);
-	}
+	
 	if(e.clientY<50&&e.clientX>(window.screen.availWidth-(.08*window.screen.availWidth))) {
 		console.log("Button ... should be added");
 		document.getElementById("fullButton").style.visibility = 'initial';
