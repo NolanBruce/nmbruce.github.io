5d740f9b0e3624c6affe8975ee344ab1632c5782
nmbruce <nmbruce@asu.edu>
2018-03-20 12:45:09 -0400

    Experimenting with how high up the chain I can grab a parent

--- TemplateData/fullscreen.js
+++ TemplateData/fullscreen.js
@@ -26,19 +26,20 @@ function goFull() {
 function addFullListener() {
 	document.getElementById("fullButton").style.visibility = 'hidden';
 	document.getElementById("gameContainer").addEventListener("click", addButton);
+	var x = document.getElementById("gameContainer");
+	var y;
+	for(int i=0;i<5;i++) {
+		y = document.getElementById(x.parentElement.nodeName);
+		console.log("Parent:  " +y);
+
+		x=document.getElementById(y);
+
+	}
 }
 
 function addButton(e) {
-	console.log("92% of screen width is at least ");
-	console.log(window.screen.availWidth-(.08*window.screen.availWidth));
 	console.log("x= " + e.clientX);
 	console.log("y= " + e.clientY);
-
-	if(e.clientY<50) {
-		console.log("High enough for button add");
-	}
-	if(e.clientX>(window.screen.availWidth-(.08*window.screen.availWidth))) {
-		console.log("Right enough for button add");
 	}
 	if(e.clientY<50&&e.clientX>(window.screen.availWidth-(.08*window.screen.availWidth))) {
 		console.log("Button ... should be added");
