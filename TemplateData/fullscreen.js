e83ec682fbddb3d7350b3832ec161cebde64fbbc
nmbruce <nmbruce@asu.edu>
2018-03-21 14:46:33 -0400

    Removing parent node search. Can't get element outside of iframe

--- TemplateData/fullscreen.js
+++ TemplateData/fullscreen.js
@@ -28,19 +28,12 @@ function addFullListener() {
 	document.getElementById("gameContainer").addEventListener("click", addButton);
 	var x = document.getElementById("gameContainer");
 	var y;
-	for(i=0;i<5;i++) {
-		y = document.getElementById(x.parentElement.nodeName);
-		console.log("Parent:  " +y);
-
-		x=document.getElementById(y);
-
-	}
 }
 
 function addButton(e) {
 	console.log("x= " + e.clientX);
 	console.log("y= " + e.clientY);
-	
+
 	if(e.clientY<50&&e.clientX>(window.screen.availWidth-(.08*window.screen.availWidth))) {
 		console.log("Button ... should be added");
 		document.getElementById("fullButton").style.visibility = 'initial';
