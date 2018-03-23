92983ca10bfc36369074d8e0973e9763d77825d0
nmbruce <nmbruce@asu.edu>
2018-03-23 15:50:15 -0400

    Moving button, cleaning up javascript

--- TemplateData/fullscreen.js
+++ TemplateData/fullscreen.js
@@ -35,16 +35,10 @@ function hideFullButton() {
 }
 
 function addButton(e) {
-	console.log("x= " + e.clientX);
-	console.log("y= " + e.clientY);
-
-	if(e.clientY<50&&e.clientX>(window.screen.availWidth-(.08*window.screen.availWidth))) {
-		console.log("Button ... should be added");
-		document.getElementById("fullButton").style.visibility = 'initial';
-	}
+	document.getElementById("fullButton").style.visibility = 'initial';
 }
 
 function defAddButton() {
 	console.log("Button ... should be added");
-		document.getElementById("fullButton").style.visibility = 'initial';
+	document.getElementById("fullButton").style.visibility = 'initial';
 }
