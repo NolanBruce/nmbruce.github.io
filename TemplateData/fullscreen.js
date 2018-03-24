cabbfe200d68448e00acf68163d181f2a52c8701
nmbruce <nmbruce@asu.edu>
2018-03-24 14:25:54 -0400

    Reverting

--- TemplateData/fullscreen.js
+++ TemplateData/fullscreen.js
@@ -1,7 +1,7 @@
 var isFull = false;
 
 function toggleFull() {
-	var frame = document.getElementById("webgl");
+	var frame = document.getElementById("gameContainer");
 	var button = document.getElementById("fullButton");
 	if (window.innerHeight != screen.height) {
 		console.log ("Attempting to enter fullscreen");
